7a9212d635a8689f726f318950c71e8ad812521e
Uri Dvir <uri.j.dvir@gmail.com>
2022-11-09 15:21:05 +0900

    Added bubble source dir

---
+++ src/bubble/bubble-count.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(int argc, char* argv[]){
+    std::cerr << "Bubble count command line program has not been implemented yet!" << std::endl;
+    return -1;
+}
---
+++ src/bubble/video-clean.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(int argc, char* argv[]){
+    std::cerr << "Video clean command line program has not been implemented yet!" << std::endl;
+    return -1;
+}
