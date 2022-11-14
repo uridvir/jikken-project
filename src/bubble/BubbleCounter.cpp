d5c709d4d03e8e5c3387bd93d8bd2781cc4d65bc
Uri Dvir <uri.j.dvir@gmail.com>
2022-11-14 17:22:26 +0900

    bubble-count does resize

--- src/bubble/BubbleCounter.cpp
+++ src/bubble/BubbleCounter.cpp
@@ -1,21 +1,47 @@
 #include "BubbleCounter.h"
 
+#include <iostream>
 #include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
 
-void analyze(cv::Mat frame){
-    
+#include <map>
+
+void analyze(cv::Mat frame, cv::Size newSize){
+    std::cout << "analyze called" << std::endl;
+    cv::imshow("Frame", frame);
+    cv::waitKey(5);
+
+    cv::Mat resized;
+    cv::resize(frame, resized, newSize, 0, 0, cv::INTER_AREA);
+    cv::imshow("Resized", resized);
+    cv::waitKey(10000); //Hold frame for 10 seconds
 }
 
 void BubbleCounter::run(cv::VideoCapture* videoIn){
-    const int analyzeCount = 10;
+    const int analyzeTotal = 10;
+
+    //TODO: FIX THIS
+    //int totalFrames = videoIn->get(cv::CAP_PROP_FRAME_COUNT);
+    int totalFrames = 2184; //Hack because frame count isn't working right now
+
+    int interval = totalFrames / analyzeTotal;
+    int frameCounter = 1, analyzeCounter = 0;
+
+    std::cout << "Interval is " << interval << " frames" << std::endl;
 
     cv::Mat frame;
-    int totalFrames = videoIn->get(cv::CAP_PROP_FRAME_COUNT);
-    int frameCounter = 0;
+    videoIn->read(frame);
+    const std::map<int, int> widthMap = {{546, 512}, {2184, 256}, {8736, 128}};
+    int newWidth = widthMap.at(totalFrames);
+    int newHeight = frame.rows * newWidth / frame.cols;
+    cv::Size newSize(newWidth, newHeight);
 
-    for (videoIn->read(frame); !frame.empty(); videoIn->read(frame)) {
-        if (frameCounter % (totalFrames / analyzeCount) == 0)
-            analyze(frame);
+    for (; !frame.empty() && analyzeCounter < analyzeTotal; videoIn->read(frame)) {
+        if (frameCounter % interval == 0){
+            std::cout << "Frame " << frameCounter << ", ";
+            analyze(frame, newSize);
+            analyzeCounter++;
+        } 
         frameCounter++;
     }
 }
