b516502f3eada6aa0216ae2765add7b9f9fc04d9
Uri Dvir <uri.j.dvir@gmail.com>
2022-11-28 15:18:59 +0900

    Started on GUI

--- src/jikken.cpp
+++ src/jikken.cpp
@@ -7,75 +7,75 @@
     #include <wx/wx.h>
 #endif
  
-class MyApp : public wxApp
+class JikkenApp : public wxApp
 {
 public:
     virtual bool OnInit();
 };
  
-class MyFrame : public wxFrame
+class JikkenFrame : public wxFrame
 {
 public:
-    MyFrame();
+    JikkenFrame();
  
 private:
-    void OnHello(wxCommandEvent& event);
+    // void OnHello(wxCommandEvent& event);
     void OnExit(wxCommandEvent& event);
     void OnAbout(wxCommandEvent& event);
 };
  
-enum
-{
-    ID_Hello = 1
-};
+// enum
+// {
+//     ID_Hello = 1
+// };
 
-wxIMPLEMENT_APP(MyApp);
+wxIMPLEMENT_APP(JikkenApp);
  
-bool MyApp::OnInit()
+bool JikkenApp::OnInit()
 {
-    MyFrame *frame = new MyFrame();
+    JikkenFrame *frame = new JikkenFrame();
     frame->Show(true);
     return true;
 }
  
-MyFrame::MyFrame()
-    : wxFrame(nullptr, wxID_ANY, "Hello World")
+JikkenFrame::JikkenFrame()
+    : wxFrame(nullptr, wxID_ANY, "Jikken じっけん")
 {
-    wxMenu *menuFile = new wxMenu;
-    menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
-                     "Help string shown in status bar for this menu item");
-    menuFile->AppendSeparator();
-    menuFile->Append(wxID_EXIT);
+    // wxMenu *menuFile = new wxMenu;
+    // menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
+    //                  "Help string shown in status bar for this menu item");
+    // menuFile->AppendSeparator();
+    // menuFile->Append(wxID_EXIT);
  
     wxMenu *menuHelp = new wxMenu;
     menuHelp->Append(wxID_ABOUT);
  
     wxMenuBar *menuBar = new wxMenuBar;
-    menuBar->Append(menuFile, "&File");
+    // menuBar->Append(menuFile, "&File");
     menuBar->Append(menuHelp, "&Help");
  
     SetMenuBar( menuBar );
  
-    CreateStatusBar();
-    SetStatusText("Welcome to wxWidgets!");
+    // CreateStatusBar();
+    // SetStatusText("じっけんちゃんから．．．こんにちは！！");
  
-    Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
-    Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
-    Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
+    // Bind(wxEVT_MENU, &JikkenFrame::OnHello, this, ID_Hello);
+    Bind(wxEVT_MENU, &JikkenFrame::OnAbout, this, wxID_ABOUT);
+    Bind(wxEVT_MENU, &JikkenFrame::OnExit, this, wxID_EXIT);
 }
  
-void MyFrame::OnExit(wxCommandEvent& event)
+void JikkenFrame::OnExit(wxCommandEvent& event)
 {
     Close(true);
 }
  
-void MyFrame::OnAbout(wxCommandEvent& event)
+void JikkenFrame::OnAbout(wxCommandEvent& event)
 {
-    wxMessageBox("This is a wxWidgets Hello World example",
-                 "About Hello World", wxOK | wxICON_INFORMATION);
+    wxMessageBox("じっけんちゃんは、じっけんちゃんが何わかんないよ～",
+                 "じっけんプログラム方法", wxOK | wxICON_INFORMATION);
 }
  
-void MyFrame::OnHello(wxCommandEvent& event)
-{
-    wxLogMessage("Hello world from wxWidgets!");
-}
+// void JikkenFrame::OnHello(wxCommandEvent& event)
+// {
+//     wxLogMessage("じっけんちゃんから．．．こんにちは！！");
+// }
