28b98497f58a39be5c0533513f1c62dd89e44901
Uri Dvir <uri.j.dvir@gmail.com>
2022-12-01 18:36:50 +0900

    Experimenting with GridBagSizer

--- src/jikken.cpp
+++ src/jikken.cpp
@@ -13,6 +13,8 @@
 #include "SettingsPanel.h"
 #include "DownloadPanel.h"
 #include "StatusPanel.h"
+
+#include <wx/gbsizer.h>
  
 class JikkenApp : public wxApp
 {
@@ -45,33 +47,23 @@ public:
         */
 
         //Panel sizers
-        wxFlexGridSizer* horizontalSizer = new wxFlexGridSizer(1, 3, wxSize(20, 0));
-        horizontalSizer->Add(configPanel, 1, wxEXPAND);
-        horizontalSizer->Add(videoPanel, 1, wxSHAPED | wxALIGN_CENTER_HORIZONTAL);
+        wxGridBagSizer* gbSizer = new wxGridBagSizer(20, 20);
+        gbSizer->Add(configPanel, wxGBPosition(0, 0), wxGBSpan(1, 1), wxEXPAND);
+        gbSizer->Add(videoPanel, wxGBPosition(0, 1), wxGBSpan(1, 1), wxSHAPED | wxALIGN_CENTER_HORIZONTAL);
 
         wxBoxSizer* rightSizer = new wxBoxSizer(wxVERTICAL);
         rightSizer->Add(settingsPanel, 1, wxEXPAND | wxTOP);
         rightSizer->AddSpacer(10);
         rightSizer->Add(downloadPanel, 0, wxEXPAND | wxBOTTOM);
 
-        horizontalSizer->Add(rightSizer, 1, wxEXPAND);
+        gbSizer->Add(rightSizer, wxGBPosition(0, 2), wxGBSpan(1, 1), wxEXPAND);
+        gbSizer->Add(statusPanel, wxGBPosition(1, 0), wxGBSpan(1, 3), wxEXPAND | wxALIGN_BOTTOM | wxALL);
 
-        horizontalSizer->AddGrowableCol(1, 1);
-        horizontalSizer->AddGrowableRow(0, 1);
-        horizontalSizer->AddGrowableCol(0, 1);
-        horizontalSizer->AddGrowableCol(2, 1);
-
-        wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
-        sizer->Add(horizontalSizer, 1, wxEXPAND);
-
-        // wxBoxSizer* verticalSizer = new wxBoxSizer(wxVERTICAL);
-        // verticalSizer->Add(horizontalSizer, 1, wxEXPAND | wxTOP);
-        // verticalSizer->AddSpacer(20);
-        // verticalSizer->Add(statusPanel, 0, wxEXPAND | wxBOTTOM);
+        gbSizer->AddGrowableCol(1, 1);
+        gbSizer->AddGrowableRow(0, 1);
 
         //Set up top panel
-        // topPanel->SetSizer(verticalSizer);
-        topPanel->SetSizer(sizer);
+        topPanel->SetSizer(gbSizer);
 
         //Frame sizer
         wxBoxSizer* frameSizer = new wxBoxSizer(wxVERTICAL);
