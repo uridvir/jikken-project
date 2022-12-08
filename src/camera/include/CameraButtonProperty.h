f28dae77ec995cbc6a151c5e5a531448419c58e0
Uri Dvir <uri.j.dvir@gmail.com>
2022-12-08 22:19:37 +0900

    Added CameraButtonProperty

---
+++ src/camera/include/CameraButtonProperty.h
@@ -0,0 +1,9 @@
+#include "CameraToggleProperty.h"
+
+class CameraButtonProperty : public CameraToggleProperty {
+public:
+    CameraButtonProperty(std::string name) : CameraToggleProperty(name, {}, nullptr) {}
+    std::vector<CameraCommand> setProperty(std::string prop, std::string value) override {
+        return canSetProperty(prop) ? std::vector<CameraCommand>({ MenuEnter }) : std::vector<CameraCommand>({});
+    }
+};
