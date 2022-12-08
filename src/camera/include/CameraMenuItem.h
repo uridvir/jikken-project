60c0314ac3e72b32885a64f178edf2f85522baae
Uri Dvir <uri.j.dvir@gmail.com>
2022-12-08 20:56:31 +0900

    CameraMenuItem methods now public

--- src/camera/include/CameraMenuItem.h
+++ src/camera/include/CameraMenuItem.h
@@ -4,6 +4,7 @@
 #include "CameraCommand.h"
 
 class CameraMenuItem {
+public:
     virtual bool canSetProperty(std::string prop) = 0;
     virtual std::vector<CameraCommand> setProperty(std::string prop, std::string value) = 0;
 };
