f9f250fdb1f1ae9645253551ed051d0d9123e96c
Uri Dvir <uri.j.dvir@gmail.com>
2022-12-14 14:33:26 +0900

    Record button now multithreaded

--- src/gui/DownloadPanel.cpp
+++ src/gui/DownloadPanel.cpp
@@ -1,63 +1,71 @@
 #include "DownloadPanel.h"
 
-DownloadPanel::DownloadPanel(wxWindow* parent, CameraController* camCtrl) : wxPanel(parent){
+#include "JikkenGlobals.h"
+
+extern JikkenGlobals jikkenGlobals;
+
+DownloadPanel::DownloadPanel(wxWindow* parent, CameraController* camCtrl) : wxPanel(parent) {
     this->camCtrl = camCtrl;
 
-    //Make elements
+    // Make elements
     recordButton = new wxButton(this, wxID_ANY, L"レコード");
     downloadButton = new wxButton(this, wxID_ANY, L"ダウンロード");
     recordButton->Enable(false);
     downloadButton->Enable(false);
 
-    //Sizer
+    // Sizer
     wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
     sizer->Add(recordButton, wxLEFT);
     sizer->Add(downloadButton, wxRIGHT);
 
-    //Setup callbacks
+    // Setup callbacks
     recordButton->Bind(wxEVT_BUTTON, &DownloadPanel::OnRecord, this);
     downloadButton->Bind(wxEVT_BUTTON, &DownloadPanel::OnDownload, this);
 
-    //Setup panel
+    // Setup panel
     SetSizer(sizer);
     Layout();
 }
 
-void DownloadPanel::OnRecord(wxCommandEvent& event){
-    //Lock
-    recordButton->Enable(false);
-    downloadButton->Enable(false);
+void DownloadPanel::OnRecord(wxCommandEvent& event) {
+    std::thread([this]() {
+        // Lock
+        recordButton->Enable(false);
+        downloadButton->Enable(false);
+        jikkenGlobals.update(MainManager::Message::LockAllCameraControls);
 
-    camCtrl->record();
+        camCtrl->record();
 
-    //TODO: Auto-download logic
+        // TODO: Auto-download logic
 
-    //Unlock
-    recordButton->Enable(true);
-    downloadButton->Enable(true);
+        // Unlock
+        jikkenGlobals.update(MainManager::Message::UnlockAllCameraControls);
+        recordButton->Enable(true);
+        downloadButton->Enable(true);
+    }).detach();
 }
 
-void DownloadPanel::OnDownload(wxCommandEvent& event){
-    //Lock
+void DownloadPanel::OnDownload(wxCommandEvent& event) {
+    // Lock
     recordButton->Enable(false);
     downloadButton->Enable(false);
 
     camCtrl->download();
 
-    //TODO: File save logic
+    // TODO: File save logic
 
-    //Unlock
+    // Unlock
     downloadButton->Enable(false);
     recordButton->Enable(true);
 }
 
-void DownloadPanel::enable(bool choice){
+void DownloadPanel::enable(bool choice) {
     recordButton->Enable(choice);
-    
+
     static bool downloadPreviouslyEnabled;
     if (!choice) {
         downloadPreviouslyEnabled = downloadButton->IsEnabled();
         downloadButton->Enable(false);
-    }
-    else downloadButton->Enable(downloadPreviouslyEnabled);
+    } else
+        downloadButton->Enable(downloadPreviouslyEnabled);
 }
